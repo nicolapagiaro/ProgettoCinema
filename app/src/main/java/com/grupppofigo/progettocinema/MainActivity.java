8a6798d73cd0950ba7320e667440e833de06fba0
halasmiftah <37208878+halasmiftah@users.noreply.github.com>
2018-03-28 17:29:36 +0200

    ho messo una variabile

--- app/src/main/java/com/grupppofigo/progettocinema/MainActivity.java
+++ app/src/main/java/com/grupppofigo/progettocinema/MainActivity.java
@@ -2,12 +2,14 @@ package com.grupppofigo.progettocinema;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
