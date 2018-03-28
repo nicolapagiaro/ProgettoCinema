84e88a63ca36bbfce2208fd005dbdbc27c2511cf
Nicola <pagiaro@gmail.com>
2018-03-28 17:35:34 +0200

    Rimuovo la variabile di salah

--- app/src/main/java/com/grupppofigo/progettocinema/MainActivity.java
+++ app/src/main/java/com/grupppofigo/progettocinema/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-TextView view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
